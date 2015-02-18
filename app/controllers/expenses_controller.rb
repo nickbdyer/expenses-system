dfde89fb327a53e1e6b46d7210c70c22e24191e1
RachelNolan <mail@rachelnolan.com>
2015-02-18 16:44:00 +0000

    Controller routes defined

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -1,11 +1,21 @@
 class ExpensesController < ApplicationController
 
   def index
-    @expenses = Expense.all
+     @expenses = Expense.all
   end
 
   def new
     @expense = Expense.new
+  end
+
+  def create
+    @expense = Expense.create(post_params)
+    redirect_to expenses_url
+  end
+
+
+  def post_params
+    params.require(:expense).permit(:image)
   end
 
 end
--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,2 +1,25 @@
 class ProjectsController < ApplicationController
+  def index
+   @projects = Project.all
+ end
+
+ def new
+   @project = Project.new
+ end
+
+ def create
+   @project = Project.create(post_params)
+   redirect_to projects_url
+ end
+
+ def destroy
+   @project = Project.find(params[:id])
+   @project.destroy
+   redirect_to projects_url
+ end
+
+
+ def post_params
+   params.require(:project).permit(:name, :atos_legal_entity, :invoice_code)
+ end
 end
