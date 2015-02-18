d6c82cbfde784d4f4bd212b0c5ed92613d3aa2bd
Pavel Redics <pavel.redics@gmail.com>
2015-02-18 14:44:41 +0000

    more tests for expenses

--- spec/features/expenses_feature_spec.rb
+++ spec/features/expenses_feature_spec.rb
@@ -5,24 +5,35 @@ feature 'Expenses' do
   context 'when logged in' do
 
     scenario 'should show a link for making a expenses' do
-      visit '/expenses'
+      visit '/'
       expect(page).to have_link("Add Expense")
     end
 
     scenario 'they should see that post and their username should be displayed' do
-      visit '/expenses'
+      visit '/'
       click_link 'Add Expense'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      fill_in 'Content', with: 'Hey everyone come see my post'
-      click_button 'Create Expense'
-      expect(page).to have_content("Hey everyone come see my post")
-      expect(page).to have_content("by test@example.com")
-      expect(current_path).to eq "/expenses"
+      expect(current_path).to eq('/expenses/new')
+      fill_in 'Date', with: '30 10 2014'
+      fill_in 'Category', with: 'Dinner'
+      fill_in 'Project', with: 'Hey everyone come see my post'
+      fill_in 'Pariticipans', with: 'Sam Tansaku'
+      click_button 'Create and add new Expense'
+      expect(current_path).to eq "/expenses/new"
     end
+
+    scenario 'they should see that post and their username should be displayed' do
+      visit '/'
+      click_link 'Add Expense'
+      expect(current_path).to eq('/expenses/new')
+      fill_in 'Date', with: '30 10 2014'
+      fill_in 'Category', with: 'Dinner'
+      fill_in 'Project', with: 'Hey everyone come see my post'
+      fill_in 'Pariticipans', with: 'Sam Tansaku'
+      click_button 'Submit and view claims'
+      expect(current_path).to eq "/claims"
+    end
+
+
 
   end
 
